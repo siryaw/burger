31c96d4b47b16e94dbb693b0062e4ecdbce8ae16
Sir Yaw <samappjnr@aol.com>
2020-04-13 23:30:18 -0400

    added db files

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(30) NOT NULL,
+    eaten BOOLEAN DEFAULT false,
+
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name, eaten)
+
+VALUES ("hamburger", false), ("cheeseburger", false), ("ham burger", false);
