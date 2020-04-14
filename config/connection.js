722d9b0c381f1ea03cb8fd7040fc7997b1a47445
Sir Yaw <samappjnr@aol.com>
2020-04-14 00:02:40 -0400

    JAWSDB connection added

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,17 @@
 // Connect Node to MySQL.
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "employee22!",
+        database: "burgers_db"
+    });
+};
+
 connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
