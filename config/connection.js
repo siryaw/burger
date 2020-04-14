8179d75713338647bd4fd6b169439a7b03258406
Sir Yaw <samappjnr@aol.com>
2020-04-13 23:55:48 -0400

    ...

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+// Connect Node to MySQL.
+var mysql = require("mysql");
+
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+// Export the connection.
+module.exports = connection;
