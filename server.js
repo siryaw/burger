92f77f1662151e94a8e1ecc2d26b4f8e1cf3baf7
Sir Yaw <samappjnr@aol.com>
2020-05-05 20:48:26 -0400

    var to const in server.js

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-//what we are requiring
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var app = express();
-var PORT = process.env.PORT || 8080;
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 
 app.use(express.static("public"));
 
-// parsing app
+p
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// parsing app
+
 app.use(bodyParser.json());
 
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller.js");
+const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
