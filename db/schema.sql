f429c72c3cadf250cf26243f14731f20d5cbfc32
Sir Yaw <samappjnr@aol.com>
2020-05-05 21:06:14 -0400

    fixed server error EADDRINUSE. changed to port 3000. additional schema edits

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,10 @@
-DROP DATABASE IF EXISTS burgers_db;
-
 CREATE DATABASE burgers_db;
-
 USE burgers_db;
-
 
 CREATE TABLE burgers
 (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    burger_name VARCHAR(30) NOT NULL,
-    eaten BOOLEAN DEFAULT false,
-
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
