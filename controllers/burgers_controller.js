d5d83a2fc2d248c954f200059a7579ba025e8d83
Sir Yaw <samappjnr@aol.com>
2020-06-17 11:45:11 -0400

    changes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,50 +5,37 @@ const burger = require("../models/burger.js");
 
 const router = express.Router();
 
-router.get("/", function (req, res) {
-    burger.selectAll(function(data) {
-        const hbsObject = {
-            burgers: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
+
+router.get("/", function(req, res) {
+    res.redirect("/burgers");
+  });
+  
+  router.get("/burgers", function(req, res) {
+    
+    burger.all(function(burgerData) {
+      
+      res.render("index", { burger_data: burgerData });
     });
-});
-
-router.post("/api/burgers", function (req, res) {
-    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
-        
-        res.json({ id: result.insertId });
+  });
+  
+  
+  router.post("/burgers/create", function(req, res) {
+    
+    burger.create(req.body.burger_name, function(result) {
+      
+      console.log(result);
+      res.redirect("/");
     });
-});
-
-router.put("/api/burgers/:id", function(req, res) {
-    const condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
-
-    burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
-        if (result.changedRows === 0) {
-           
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+  });
+  
+  
+  router.put("/burgers/:id", function(req, res) {
+    burger.update(req.params.id, function(result) {
+     
+      console.log(result);
+      
+      res.sendStatus(200);
     });
-});
-
-router.delete("/api/burgers/:id", function(req, res) {
-    const condition = "id = " + req.params.id;
-    console.log("condition", condition);
-
-    burger.deleteOne(condition, function(result) {
-        if (result.changedRows === 0) {
-            
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-});
+  });
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -9,24 +9,20 @@ var burger = {
         });
     },
    
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function(res) {
-            cb(res);
-        });
-    },
-    
-    updateOne: function(objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function(res) {
-            cb(res);
-        });
-    },
-  
-    deleteOne: function(condition, cb) {
-        orm.deleteOne("burgers", condition, function(res) {
-            cb(res);
-        });
-    }
-};
+    create: function(name, cb) {
+        orm.create("burgers", [
+          "burger_name", "devoured"
+        ], [
+          name, false
+        ], cb);
+      },
+      update: function(id, cb) {
+        var condition = "id=" + id;
+        orm.update("burgers", {
+          devoured: true
+        }, condition, cb);
+      }
+    };
 
 
 module.exports = burger;
